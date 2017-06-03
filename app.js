18e64df06cd7329cdf013426417f1ec2ac4e4516
Timothy Cullen <timothycullen@Timothys-MacBook-Pro.local>
2017-06-03 13:57:56 -0700

    refractor file

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+//REFACTOR THIS Entire file, with questions that return results based on the
+//content of the opening page. Add doc write methods as well as Loops!
+// var user = prompt('');
+//
+//
+var question1 = prompt('?')
+        if(question1 === 'Y') {
+          alert( '');
+         console.log('');
+       }else{
+          alert('');
+
+ var question2 = prompt('?');
+       if(question2 === 'Y') {
+        alert('')
+         console.log('');
+      }else{
+        alert('?');
+         console.log('H');
